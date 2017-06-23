0f34b00663ad7c043d158f5b87ce627bdc21628e
KimberllyMoreles <kim.moreles@hotmail.com>
2017-06-23 01:40:06 -0300

    Descobrir posições em andamento

--- Jogo.php
+++ Jogo.php
@@ -8,6 +8,10 @@
 		array_push($posicoesFechadas, $posicao);
 	}
 	
+	function removerDasPosicoesAbertas($posicao, &$posicoesAbertas){	
+		array_splice($posicoesAbertas, array_search($posicao, $posicoesAbertas), 1);
+	}
+	
 	function inicializarListas($posicaoAtual, $obstaculos, &$posicoesAbertas, &$posicoesFechadas){
 		adicionarNasPosicoesAbertas($posicaoAtual, $posicoesAbertas);
 		
@@ -17,19 +21,31 @@
 		
 	}	
 	
-	function criarPosicoesAbertas($posicaoAtual, $obstaculos){
-		verificarObstaculos($obstaculos, $posicaoCandidata);
-		return adicionarNasPosicoesAbertas($posicaoCandidata);
+	function criarPosicoesAbertas($posicaoAtual, $posicoesFechadas, &$posicoesAbertas){
+		$posicoesCandidatas = array(4);
+		$posicoesCandidatas[0] = [$posicaoAtual['posicaoX'] - 1, $posicaoAtual['posicaoY']];
+		$posicoesCandidatas[1] = [$posicaoAtual['posicaoX'] + 1, $posicaoAtual['posicaoY']];
+		$posicoesCandidatas[2] = [$posicaoAtual['posicaoX'], $posicaoAtual['posicaoY'] - 1];
+		$posicoesCandidatas[3] = [$posicaoAtual['posicaoX'], $posicaoAtual['posicaoY'] + 1];
+		
+		for ($i =0; $i <=count ($posicoesCandidatas) - 1; $i++) {
+			validarPosicoesCandidatas($posicoesFechadas, $posicoesCandidatas[$i], $posicoesAbertas);
+		}
 	}
 	
-	function verificarObstaculos($obstaculos, $posicaoCandidata){
-	
+	function validarPosicoesCandidatas($posicoesFechadas, $posicaoCandidata, &$posicoesAbertas){	
+		for($i=0; $i<sizeof($posicoesFechadas); $i++){
+			if($posicaoCandidata != $posicoesFechadas[$i]){
+				adicionarNasPosicoesAbertas($posicaoCandidata, $posicoesAbertas);
+			}		
+		}
 	}
 	
-	function descobrirMelhorPosicaoAberta(&$posicoesAbertas, $objetivo){
+	function descobrirMelhorPosicaoAberta(&$posicoesAbertas, $objetivo, $posicaoAtual){
 		$custos = array();
-		for($i=0; $i<sizeof($posicoesAbertas); $i++){		
-			array_push($custos, custo($posicoesAbertas[$i], $objetivo));								
+		for($i=0; $i<sizeof($posicoesAbertas); $i++){	
+			if($posicoesAbertas[$i] != $posicaoAtual)
+				array_push($custos, custo($posicoesAbertas[$i], $objetivo));								
 		}
 		return array_search(min($custos), $custos);			
 	}
@@ -38,8 +54,10 @@
 		return abs($objetivo['posicaoX'] - $posicaoAtual['posicaoX']) + abs($objetivo['posicaoY'] - $posicaoAtual['posicaoY']);
 	}
 	
-	function moverAgente($posicaoAtual, $melhorPosicaoAberta){
-	
+	function moverAgente(&$posicaoAtual, $melhorPosicaoAberta, &$posicoesAbertas, &$posicoesFechadas){
+		adicionarNasPosicoesFechadas($posicaoAtual, $posicoesFechadas);
+		removerDasPosicoesAbertas($posicaoAtual, $posicoesAbertas);
+		$posicaoAtual = $melhorPosicaoAberta;
 	}
 	
 	$posicoesAbertas = array();
@@ -53,9 +71,7 @@
 	$objetivo = array('posicaoX'=>7, 'posicaoY'=>1);	
 	inicializarListas($posicaoAtual, $obstaculos, $posicoesAbertas, $posicoesFechadas);	
 	
-	$custo = custo($posicaoAtual, $objetivo);
-
-	$melhorPosicaoAberta = $posicoesAbertas[descobrirMelhorPosicaoAberta($posicoesAbertas,  $objetivo)];
-	/*
-	moverAgente($posicaoAtual, $melhorPosicaoAberta);
-*/
+	criarPosicoesAbertas($posicaoAtual, $posicoesFechadas, $posicoesAbertas);	
+	$melhorPosicaoAberta = $posicoesAbertas[descobrirMelhorPosicaoAberta($posicoesAbertas,  $objetivo, $posicaoAtual)];
+	
+	moverAgente($posicaoAtual, $melhorPosicaoAberta, $posicoesAbertas, $posicoesFechadas);
